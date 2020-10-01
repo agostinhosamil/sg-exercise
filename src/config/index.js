b3986cffe31f0acfc737bce52e0ea523267ca675
agostinhosamil <agostinhosaml832@gmail.com>
2020-10-01 02:52:06 -0700

    Update application main configuration file and package.json

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,6 @@ const appConfig = package.ApplicationConfigurations || {
 	port: null
 }
 
-
 for (let key in package) {
 	if (configKeysRe.test(key)) {
 		let appConfigProp = key.replace (configKeysRe, '')
@@ -14,6 +13,12 @@ for (let key in package) {
 			package [ key ]
 		)
 	}
+}
+
+if (!appConfig.port || process.env.PORT) {
+	appConfig.port = process.env.PORT || (
+		appConfig.port || 3333
+	)
 }
 
 export default appConfig
